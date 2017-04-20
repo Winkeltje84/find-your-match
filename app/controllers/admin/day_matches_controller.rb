15158149daaae2a82c27434c84d3fa1cfe4ffd4f
Winkeltje84 <bwinkel@gmail.com>
2017-04-20 19:02:27 +0200

    Started CreateMatches method in DayMatch model and fixed some seeds . After a lot of problems....

--- app/controllers/admin/day_matches_controller.rb
+++ app/controllers/admin/day_matches_controller.rb
@@ -8,9 +8,27 @@ class Admin::DayMatchesController < ApplicationController
     # debugger
   end
 
+  def create
+    # called from view to create a new set of day_matches
+    debugger
+    # current_day = Date.today
+    # create_day_matches(current_day)
+    create_day_matches
+  end
+
+  def create_day_matches
+    debugger
+  end
+
+  def new
+    # called for every new day_matches line
+  end
+
+
   private
 
   def user_is_admin?
     redirect_to :root unless current_user && current_user.admin?
   end
+
 end
--- app/models/day_match.rb
+++ app/models/day_match.rb
@@ -1,4 +1,8 @@
 class DayMatch < ApplicationRecord
   belongs_to :student
   belongs_to :matched_student
+
+  def self.create_day_matches
+    debugger
+  end
 end
