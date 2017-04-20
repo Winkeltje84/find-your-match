a9abb1d14360b75908ff0d85f0a661135b3cd6e5
Winkeltje84 <bwinkel@gmail.com>
2017-04-20 10:44:33 +0200

    Added controller DayMatches into admin and created routes and link

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  
+
   devise_for :users
 
   resources :students, only: [:new, :edit, :create, :update]
 
   namespace :admin do
       resources :users
+      resources :day_matches
   end
 end
---
+++ spec/controllers/day_matches_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DayMatchesController, type: :controller do
+
+end
