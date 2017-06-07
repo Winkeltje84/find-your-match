ae9d3eb4c5cf25e9615ae5c18064654f2ce83a62
Winkeltje84 <bwinkel@gmail.com>
2017-06-07 15:46:21 +0200

    refactored "get_current_user_matches" method from controller to model

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,17 @@ class Student < ApplicationRecord
   has_many :day_matches
 
   validates :name, presence: true
+
+  def self.get_current_user_matches(student)
+    debugger
+    day_match_array = []
+    day_matches = DayMatch.all
+    day_matches.each do |match|
+      if match.student_id === student.id
+        day_match_array.push(match)
+      end
+    end
+    return day_match_array
+  end
+  
 end
