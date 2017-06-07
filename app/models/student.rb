bc10a6e7d93f327766c5cf1c38c18a8980d799ab
Winkeltje84 <bwinkel@gmail.com>
2017-06-07 17:23:14 +0200

    started "matched_student_name" variable to show in student show page: to show the names of his/her matches - but currently not yet used

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,15 +5,22 @@ class Student < ApplicationRecord
   validates :name, presence: true
 
   def self.get_current_user_matches(student)
-    debugger
     day_match_array = []
     day_matches = DayMatch.all
     day_matches.each do |match|
       if match.student_id === student.id
+        matched_student_name = get_matched_student_name(match.matched_student_id) #this 'matched_student_name' is still unused
         day_match_array.push(match)
       end
     end
+    debugger
     return day_match_array
   end
-  
+
+  def self.get_matched_student_name(matched_student_id)
+    debugger
+    matched_student_name = Student.select{ |student| student.id == matched_student_id}[0].name
+    return matched_student_name
+  end
+
 end
